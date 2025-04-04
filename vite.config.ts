c4be45298c53245f11101d16c0aeb0f8f01bd81e
Gaurav Tyagi <gauravtyagi@berkeley.edu>
2025-04-03 18:03:52 -0700

    fix: Update configuration for custom domain support

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [solid()],
-  base: command === 'serve' ? '/' : '/website/',
-}))
+  base: '/',
+  build: {
+    target: 'esnext'
+  }
+})
