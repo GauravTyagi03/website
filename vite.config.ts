3479e0a2513221ad01762b6c9b398a10199cb19b
Gaurav Tyagi <gauravtyagi@berkeley.edu>
2025-04-03 17:42:07 -0700

    fix: Update build and deployment configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => ({
   plugins: [solid()],
-  base: '/website/', // GitHub repository name
-})
+  base: command === 'serve' ? '/' : '/website/',
+}))
